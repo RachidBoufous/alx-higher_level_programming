e585678cdfa91f46faaa9db6181f6713a50e23dc
RachidBoufous <rachidboufous32gmail.com>
2023-05-11 21:50:39 +0000

    task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+# importing the add function from add_0.py file
+from add_0 import add
+
+# assigning values to a and b variables
+a = 1
+b = 2
+
+# calling the add function with a and b as arguments and storing the result in a variable
+result = add(a, b)
+
+# printing the output in the required format
+print(f"{a} + {b} = {result}\n")
