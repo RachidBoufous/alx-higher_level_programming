f14c0980cf5b2ce774b1320ba89d88fb9089e303
RachidBoufous <rachidboufous32gmail.com>
2023-05-15 19:20:36 +0000

    task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """a function that returns an element
+    at a given index
+    Args:
+        my_list (list): list of integers
+        idx (integer): index to locate an element in list
+    """
+
+    if idx < 0 or idx > len(my_list):
+        return None
+    else:
+        return my_list[idx]
--- 0x03-python-data_structures/README.md
+++ 0x03-python-data_structures/README.md
@@ -1,1 +1,2 @@
 0. print all the integers in a list
+1. function to get an item from a given index in list
