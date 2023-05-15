cdd51f13c6c645eb320c215e120bb3e560ea52ee
RachidBoufous <rachidboufous32gmail.com>
2023-05-15 20:18:21 +0000

    task 5 fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 
 def no_c(my_string):
     """return string after striping all the C's in it
@@ -7,5 +7,8 @@ def no_c(my_string):
         my_string (string): string with c characters
     """
 
-    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    new_string = ""
+    for x in my_string:
+        if x != 'c' and x != 'C':
+            new_string += x
     return new_string
