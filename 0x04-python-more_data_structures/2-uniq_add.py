c6a9cc7915caa051538bef7aa0d2549aceb488e9
RachidBoufous <rachidboufous32gmail.com>
2023-05-17 20:49:49 +0000

    fixing style

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,7 +4,8 @@ def uniq_add(my_list=[]):
     """sum of unique items
 
     Args:
-        my_list (list of integers, optional): list of integers, maybe repeated. Defaults to [].
+        my_list (list of integers, optional): list of integers,
+        maybe repeated. Defaults to [].
 
     Returns:
         int: the result of the sum of unique items
