77a520fa54994ce73e5f2e22f560afb32664faef
RachidBoufous <rachidboufous32gmail.com>
2023-05-17 21:43:40 +0000

    task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -10,4 +10,4 @@ def multiply_list_map(my_list=[], number=0):
     Returns:
         list: updated list
     """
-    return list(map(lambda x: x * number, my_list))
+    return (list(map((lambda i: i * number), my_list)))
