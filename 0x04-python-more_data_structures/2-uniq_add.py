a5e78c54fb4fbccdcfe2b47f4769cd5fabcc7702
RachidBoufous <rachidboufous32gmail.com>
2023-05-17 20:47:19 +0000

    task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    """sum of unique items
+
+    Args:
+        my_list (list of integers, optional): list of integers, maybe repeated. Defaults to [].
+
+    Returns:
+        int: the result of the sum of unique items
+    """
+    set1 = set(my_list)
+    sum = 0
+    for i in set1:
+        sum += i
+    return sum
