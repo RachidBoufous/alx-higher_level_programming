a11286bbfd70f3f175f76bff6c4b8dae0f99978d
RachidBoufous <rachidboufous32gmail.com>
2023-05-17 21:45:51 +0000

    task 11 fix

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    """multiply all elements of a list
-
-    Args:
-        my_list (list, optional): list. Defaults to [].
-        number (int, optional): number to multiply. Defaults to 0.
-
-    Returns:
-        list: updated list
-    """
     return (list(map((lambda i: i * number), my_list)))
