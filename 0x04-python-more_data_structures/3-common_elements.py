6aa7067812bcd5431366f2b7433ea96a69ae8f60
RachidBoufous <rachidboufous32gmail.com>
2023-05-17 20:53:17 +0000

    task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """return common elements in both sets
+
+    Args:
+        set_1 (set): set 1 
+        set_2 (set): set 2
+
+    Returns:
+        list: commun elements
+    """
+
+    communItems = []
+    for i in set_1:
+        if i in set_2:
+            communItems.append(i)
+    return communItems
