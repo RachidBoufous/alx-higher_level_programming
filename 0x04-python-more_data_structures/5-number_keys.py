34a90e99b7ba5f2ee5a2883dab86855536e4c3c7
RachidBoufous <rachidboufous32gmail.com>
2023-05-17 21:27:12 +0000

    task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """return number of keys in a dictionary
+
+    Args:
+        a_dictionary (dict): dictionary
+
+    Returns:
+        int: number of keys
+    """
+    return len(a_dictionary.keys())
