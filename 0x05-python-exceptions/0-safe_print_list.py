f1f82e2fdca0888e4fe582a56d049e917543abe5
RachidBoufous <rachidboufous32gmail.com>
2023-05-29 21:17:40 +0000

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+        print()
+        return i + 1
+    except IndexError:
+        print()
+        return i
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+    0. print the elements in the list
